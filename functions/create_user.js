f595ba5db49e0a383e54b3e7c92950deef9d9635
Doug Cole <idugcoal@gmail.com>
2018-03-14 22:48:09 -0700

    Adds createUser firebase cloud function

---
+++ functions/create_user.js
@@ -0,0 +1,15 @@
+const admin = require('firebase-admin')
+
+module.exports = function (req, res) {
+  // Verify the user provided a phone number
+  if (!req.body.phone) {
+    return res.status(422).send({ error: "Bad input" })
+  }
+  // Format phone number to remove dashes and parens
+  const phone = String(req.body.phone).replace(/[^\d]/g, "")
+  // Create a new user account using the phone number
+  admin.auth().createUser({ uid: phone })
+  // Respond to the user request saying the account was made
+    .then(user => res.send(user))
+    .catch(err => res.status(422).send({ error: err }))
+}
